22a628bb94aec85596ce5faaf2a8937014a68c1a
Aymeric Dijoux <aymeric@MacBook-Pro-de-admin-6.local>
2019-10-12 00:23:50 +0200

    feat/init: add server.js

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,1 +1,17 @@
 const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async(req, res) => {
+  return { hello: 'world' };
+});
+
+ async function start(){
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit();
+  }
+}
+
+start();
