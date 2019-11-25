25ff237599e3994492d982ab2935f94d87947892
Aymeric Dijoux <aymeric@MacBook-Pro-de-admin-6.local>
2019-11-25 23:03:00 +0100

    add created_at updated_at on insert

--- src/modules/auth/auth.repository.js
+++ src/modules/auth/auth.repository.js
@@ -1,8 +1,8 @@
 function authRepository(database) {
   function upsertCredentials(id, password) {
     return database.query(`
-      INSERT INTO credentials (customer_id, password)
-      VALUES ($1, $2)
+      INSERT INTO credentials (customer_id, password, created_at, updated_at)
+      VALUES ($1, $2, NOW(), NOW())
       ON CONFLICT (customer_id) DO UPDATE
       SET password = $3, updated_at = NOW()
     `, [id, password, password]);
