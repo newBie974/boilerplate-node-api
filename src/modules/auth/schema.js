39816ae3a4bcac3c96981ff83be1f85a43865823
Aymeric Dijoux <aymeric@MacBook-Pro-de-admin-6.local>
2019-12-01 01:56:21 +0100

    todo schema

--- src/modules/auth/schema.js
+++ src/modules/auth/schema.js
@@ -9,10 +9,11 @@ function verifyRequiredConfig(configList) {
     }],
   };
   Object.keys(configList).forEach((el) => {
+    /* eslint valid-typeof: "error" */
     if (!schema.required[el]) {
       throw new Error(`[SCHEMA_FAILED] ${el} value is not set`);
     } else if (schema.required[el]
-      && typeof el !== 'schema.required[el]') {
+      && typeof el !== schema.required[el]) {
       throw new Error(`[SCHEMA_FAILED] ${el} should be typeof ${schema.required[el]}`);
     }
   });
